c2d2784ec8d2681ebcac9b99c4ca9486557d4818
crystal <Crystal@Crystals-MacBook-Pro.local>
2015-10-12 20:01:54 -0700

    First commit

--- app/src/main/java/com/codepath/apps/mysimpletweets/TwitterClient.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/TwitterClient.java
@@ -27,8 +27,8 @@ import com.loopj.android.http.RequestParams;
 public class TwitterClient extends OAuthBaseClient {
     public static final Class<? extends Api> REST_API_CLASS = TwitterApi.class; // Change this
     public static final String REST_URL = "https://api.twitter.com/1.1"; // Change this, base API URL
-    public static final String REST_CONSUMER_KEY = "weQBSdPuO0z5iWNCSItPog4NR";       // Change this
-    public static final String REST_CONSUMER_SECRET = "Kn7ZOPBrUlvLGJ4u9FUzTpIddAcxBnpOmxs8j0SoXgUHbSJAHi"; // Change this // Change this
+    public static final String REST_CONSUMER_KEY = "xxxxxxxx";       // Change this
+    public static final String REST_CONSUMER_SECRET = "xxxxxx"; // Change this // Change this
     public static final String REST_CALLBACK_URL = "oauth://cpsimpletweets"; // Change this (here and in manifest)
 
     public TwitterClient(Context context) {
